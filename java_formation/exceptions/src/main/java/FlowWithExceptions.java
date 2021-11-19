03b0a88aaf7c1a4a33a5cadd657a50cf6e53df93
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-11-19 12:51:48 -0300

    Created custom exception class, and start testing system errors

--- java_formation/exceptions/src/main/java/FlowWithExceptions.java
+++ java_formation/exceptions/src/main/java/FlowWithExceptions.java
@@ -1,9 +1,11 @@
-public class Flow {
+import bank.MyException;
+
+public class FlowWithExceptions {
     public static void main(String[] args) {
         System.out.println("Start of main");
         try {
         method1();
-        }catch (ArithmeticException | NullPointerException e){
+        }catch (MyException | NullPointerException e){
             System.out.println(e);
         }
         System.out.println("End of main");
@@ -17,12 +19,8 @@ public class Flow {
 
     private static void method2() {
         System.out.println("Start of method2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-                //var a = 1/0;
-                User c = null;
-                c.setName();
-        }
-        System.out.println("End of method2");
+
+        throw new MyException("RIP");
+        //System.out.println("End of method2");
     }
 }
---
+++ java_formation/exceptions/src/main/java/bank/MyException.java
@@ -0,0 +1,7 @@
+package bank;
+
+public class MyException extends RuntimeException{
+    public MyException(String message) {
+        super(message);
+    }
+}
