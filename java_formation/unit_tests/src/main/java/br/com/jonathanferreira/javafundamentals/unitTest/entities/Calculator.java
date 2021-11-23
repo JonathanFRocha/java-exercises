7e5f5a4a2d552495ba4f505dba6850a2fcae53cd
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-11-22 21:24:33 -0300

    Testing Sum

---
+++ java_formation/unit_tests/src/main/java/br/com/jonathanferreira/javafundamentals/unitTest/entities/Calculator.java
@@ -0,0 +1,7 @@
+package br.com.jonathanferreira.javafundamentals.unitTest.entities;
+
+public class Calculator {
+    public int calculate(int a, int b) {
+        return a+b;
+    }
+}
---
+++ java_formation/unit_tests/src/main/java/br/com/jonathanferreira/javafundamentals/unitTest/entities/CalculatorTest.java
@@ -0,0 +1,15 @@
+package br.com.jonathanferreira.javafundamentals.unitTest.entities;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void shouldSumTwoIntegerNumbers(){
+        var c = new Calculator();
+        int sum = c.calculate(1, 2);
+        Assertions.assertEquals(3 ,sum);
+    }
+}
