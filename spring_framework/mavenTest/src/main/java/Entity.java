e0f17e38f2630a0314996d62fd8fe482e3380a9b
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-11-26 15:50:48 -0300

    finished course

---
+++ spring_framework/mavenTest/src/main/java/Entity.java
@@ -0,0 +1,18 @@
+public class Entity {
+    private String anything;
+
+    public Entity() {
+    }
+
+    public Entity(String anything) {
+        this.anything = anything;
+    }
+
+    public String getAnything() {
+        return anything;
+    }
+
+    public void setAnything(String anything) {
+        this.anything = anything;
+    }
+}
