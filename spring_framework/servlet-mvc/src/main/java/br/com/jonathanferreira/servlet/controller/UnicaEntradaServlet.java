3514dcfc76c31c1474d1424d0a1cce2d496386ab
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-11-26 12:54:22 -0300

    finished

--- spring_framework/servlet-mvc/src/main/java/br/com/jonathanferreira/servlet/controller/UnicaEntradaServlet.java
+++ spring_framework/servlet-mvc/src/main/java/br/com/jonathanferreira/servlet/controller/UnicaEntradaServlet.java
@@ -4,7 +4,6 @@ import br.com.jonathanferreira.servlet.acoes.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
