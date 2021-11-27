d5368a6bba2333a7d6c06ffb78659e1fa7ff3b18
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-11-27 14:04:39 -0300

    cleaning unused imports

--- spring_framework/ecommerce-repository/src/main/java/br/com/jonathanferreira/jdbc/TestCategoryList.java
+++ spring_framework/ecommerce-repository/src/main/java/br/com/jonathanferreira/jdbc/TestCategoryList.java
@@ -1,12 +1,9 @@
 package br.com.jonathanferreira.jdbc;
 
 import br.com.jonathanferreira.jdbc.dao.CategoryDAO;
-import br.com.jonathanferreira.jdbc.dao.ProductDAO;
 import br.com.jonathanferreira.jdbc.model.Category;
-import br.com.jonathanferreira.jdbc.model.Product;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
 public class TestCategoryList {
