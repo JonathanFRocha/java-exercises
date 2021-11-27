868d560afff91ece789bc86ca3089eef466a14e4
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-11-27 09:58:42 -0300

    using prepared statement to avoid SQL Injection

--- spring_framework/ecommerce-repository/src/main/java/br/com/jonathanferreira/jdbc/TestInsertionWithParams.java
+++ spring_framework/ecommerce-repository/src/main/java/br/com/jonathanferreira/jdbc/TestInsertionWithParams.java
@@ -1,4 +1,28 @@
 package br.com.jonathanferreira.jdbc;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class TestInsertionWithParams {
+    public static void main(String[] args) {
+        try (Connection conn = new ConnectionFactory().getConnection()) {
+            String name = "mouse'";
+            String descriptions = "mouse sem fio): delete from product;";
+            String sql = "INSERT INTO product (name, description) VALUES ( ?, ?)";
+            var pstm = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            pstm.setString(1, name);
+            pstm.setString(2, descriptions);
+
+            pstm.execute();
+
+            var rst = pstm.getGeneratedKeys();
+            while (rst.next()) {
+                int id = rst.getInt(1);
+                System.out.println("item inserted has id: " + id);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
