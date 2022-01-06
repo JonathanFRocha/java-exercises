1804ad6ecdf907a6dfd26e244b203b9ee049ba2f
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2022-01-05 22:36:49 -0300

    adding profiles

--- spring_framework/forum/src/main/java/br/com/jonathanferreira/spring/forum/config/security/SecurityConfigurations.java
+++ spring_framework/forum/src/main/java/br/com/jonathanferreira/spring/forum/config/security/SecurityConfigurations.java
@@ -4,6 +4,7 @@ import br.com.jonathanferreira.spring.forum.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -17,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @EnableWebSecurity
 @Configuration
+@Profile("prod")
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -41,6 +43,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/topics/*").permitAll()
                 .antMatchers(HttpMethod.POST,"/auth").permitAll()
                 .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/topics/*").hasRole("MODERATOR")
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
--- spring_framework/forum/src/main/java/br/com/jonathanferreira/spring/forum/controller/AuthenticationController.java
+++ spring_framework/forum/src/main/java/br/com/jonathanferreira/spring/forum/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import br.com.jonathanferreira.spring.forum.config.security.TokenService;
 import br.com.jonathanferreira.spring.forum.controller.dto.TokenDto;
 import br.com.jonathanferreira.spring.forum.controller.form.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,6 +19,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
+@Profile("prod")
 public class AuthenticationController {
 
     @Autowired
